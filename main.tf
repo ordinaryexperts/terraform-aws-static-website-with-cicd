2755fefbb7302bc1d1572765db89d3370dbc7ad9
Dylan Vaughn <dylan@ordinaryexperts.com>
2021-03-30 14:34:06 -0700

    fixing Terraform 12 syntax issues

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_waf_ipset" "whitelisted_ips" {
-  count = "${length(var.whitelisted_ips) > 0 ? 1 : 0}"
+  count = length(var.whitelisted_ips) > 0 ? 1 : 0
   name = "WhitelistedIps"
 
   dynamic "ip_set_descriptors" {
@@ -14,8 +14,8 @@ resource "aws_waf_ipset" "whitelisted_ips" {
 }
 
 resource "aws_waf_rule" "whitelisted_ips_rule" {
-  count = "${length(var.whitelisted_ips) > 0 ? 1 : 0}"
-  depends_on  = ["aws_waf_ipset.whitelisted_ips"]
+  count = length(var.whitelisted_ips) > 0 ? 1 : 0
+  depends_on  = [aws_waf_ipset.whitelisted_ips]
   name        = "${var.env}WhitelistedIPsRule"
   metric_name = "${var.env}WhitelistedIPsRule"
 
@@ -27,8 +27,8 @@ resource "aws_waf_rule" "whitelisted_ips_rule" {
 }
 
 resource "aws_waf_web_acl" "whitelisted_ips_acl" {
-  count = "${length(var.whitelisted_ips) > 0 ? 1 : 0}"
-  depends_on  = ["aws_waf_rule.whitelisted_ips_rule"]
+  count = length(var.whitelisted_ips) > 0 ? 1 : 0
+  depends_on  = [aws_waf_rule.whitelisted_ips_rule]
   name        = "${var.env}WhitelistedIPsACL"
   metric_name = "${var.env}WhitelistedIPsACL"
 
@@ -50,16 +50,16 @@ resource "aws_waf_web_acl" "whitelisted_ips_acl" {
 resource "aws_cloudformation_stack" "website_bucket_and_cf" {
   name = "${var.env}-website-bucket-and-cf-stack"
   capabilities = ["CAPABILITY_IAM"]
-  depends_on = ["aws_waf_web_acl.whitelisted_ips_acl"]
+  depends_on = [aws_waf_web_acl.whitelisted_ips_acl]
   on_failure = "DELETE"
   parameters = {
     CertificateArn = var.cert_arn
     CustomErrorResponsePagePath = var.custom_error_response_page_path
     Debug = var.debug
     Domain = var.domain
-    WebACLId = "${length(var.whitelisted_ips) > 0 ? "${join("", aws_waf_web_acl.whitelisted_ips_acl.*.id)}" : "none"}"
+    WebACLId = length(var.whitelisted_ips) > 0 ? "${join("", aws_waf_web_acl.whitelisted_ips_acl.*.id)}" : "none"
   }
-  template_body = "${file("${path.module}/website_bucket_and_cf.yaml")}"
+  template_body = file("${path.module}/website_bucket_and_cf.yaml")
   # CloudFront distributions can take a long time to create...
   timeouts {
     create = "2h"
@@ -72,12 +72,12 @@ resource "aws_cloudformation_stack" "website_bucket_and_cf" {
 resource "aws_cloudformation_stack" "pipeline_bucket" {
   name = "${var.env}-website-pipeline-bucket-stack"
   on_failure = "DELETE"
-  template_body = "${file("${path.module}/pipeline_bucket.yaml")}"
+  template_body = file("${path.module}/pipeline_bucket.yaml")
 }
 
 resource "aws_cloudformation_stack" "website_cicd" {
   capabilities = ["CAPABILITY_IAM"]
-  depends_on = ["aws_cloudformation_stack.website_bucket_and_cf"]
+  depends_on = [aws_cloudformation_stack.website_bucket_and_cf]
   name = "${var.env}-website-cicd-stack"
   on_failure = "DELETE"
   parameters = {
@@ -91,5 +91,5 @@ resource "aws_cloudformation_stack" "website_cicd" {
     SourceRepoName = var.repo_name
     WebsiteBucket = aws_cloudformation_stack.website_bucket_and_cf.outputs["WebsiteBucket"]
   }
-  template_body = "${file("${path.module}/website_cicd.yaml")}"
+  template_body = file("${path.module}/website_cicd.yaml")
 }
